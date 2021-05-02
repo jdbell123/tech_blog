9f08b598d458d06a75cfcaf57eca3a2b70ec700b
John Bell <jdbell123@gmail.com>
2021-05-01 21:33:28 -0400

    created models

---
+++ models/index.js
@@ -0,0 +1,36 @@
+const User = require('./User');
+const Blogs = require('./Blogs');
+const Comments = require('./Comments');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Blogs, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blogs.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comments, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comments.belongsTo(Blogs, {
+  foreignKey: 'blog_id'
+})
+
+Blogs.hasMany(Comments, {
+  foreignKey: 'blog_id',
+})
+
+module.exports = { User, Blogs, Comments };
