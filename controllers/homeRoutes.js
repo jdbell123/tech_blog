eabc9718df493fbd65a64f164db3bcee5ce40f2f
John Bell <jdbell123@gmail.com>
2021-05-01 21:55:35 -0400

    created homeRoutes file

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const { User, Blogs, Comments } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    console.log("I'm here!");
+    // Get all projects and JOIN with user data
+    const blogsData = await Blogs.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    // const projects = projectData.map((project) => project.get({ plain: true }));
+    const blogs = blogsData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      blogs,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const blogsData = await Blogs.findAll({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Comments,
+          include: [{ model: User, attributes: ['name'] }]
+
+        },
+      ],
+    });
+
+    const blogs = blogsData.map((blogs) => blogs.get({ plain: true }));
+    res.render('blog', {
+      blog: blogs[0],
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Blogs }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/Create', withAuth, async (req, res) => {
+  try {
+    res.render('create', {
+      logged_in: req.session.logged_in
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
